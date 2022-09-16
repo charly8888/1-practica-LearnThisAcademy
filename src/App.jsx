21abb3fd55ded669c93b39a2650e5c9faf614b97
German Hornus <hornusgerman@gmail.com>
2022-09-16 08:48:06 -0500

    user list refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
 import UserList from './components/UserList';
+import { USER_ROLES } from './constants/userRoles';
 
 const USERS = [
 	{
 		username: 'german',
 		name: 'German Hornus',
-		role: 'other',
+		role: USER_ROLES.OTHER,
 		active: true
 	},
 	{
 		username: 'jose',
 		name: 'Lalo Hornus',
-		role: 'student',
+		role: USER_ROLES.STUDENT,
 		active: true
 	},
 	{
 		username: 'eze',
 		name: 'Ezequiel Hornus',
-		role: 'teacher',
+		role: USER_ROLES.TEACHER,
 		active: false
 	},
 	{
 		username: 'germanchah',
 		name: 'Geasdnus',
-		role: 'other',
+		role: USER_ROLES.OTHER,
 		active: true
 	},
 	{
 		username: 'joasde',
 		name: 'Lalo Hornus',
-		role: 'student',
+		role: USER_ROLES.STUDENT,
 		active: false
 	},
 	{
 		username: 'edase',
 		name: 'Ezequiel Hornus',
-		role: 'teacher',
+		role: USER_ROLES.TEACHER,
 		active: false
 	}
 ];
